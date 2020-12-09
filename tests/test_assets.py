d6e4d71fd3474baf4a823650fcea61b31f90cc73
Anthony Chen <31290848+ComputerGenius152@users.noreply.github.com>
2020-12-09 00:19:47 -0500

    Update test_assets.py
    make it definitively assert for 200 instead of detecting not 404

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,13 +1,13 @@
 def test_assets(client):
     '''Test to ensure assets are visible.'''
     result = client.get('/assets/css/style.css')
-    assert result.status_code != 404
+    assert result.status_code == 200
     assert b'Page Not Found' not in result.data
     
     result = client.get('/assets/images/trash.svg')
-    assert result.status_code != 404
+    assert result.status_code == 200
     assert b'Page Not Found' not in result.data
     
     result = client.get('/assets/js/dateconvert.js')
-    assert result.status_code != 404
+    assert result.status_code == 200
     assert b'Page Not Found' not in result.data
