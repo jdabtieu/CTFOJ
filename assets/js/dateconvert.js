9481270057d1288cb555a09f681792efafa7f110
jdabtieu <jonathan.wu3@outlook.com>
2020-12-08 23:46:50 -0500

    fix mix of tabs & spaces

--- assets/js/dateconvert.js
+++ assets/js/dateconvert.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".dt").forEach(function (e) {
-	var split = e.innerHTML.split(" ");
-		var date_split = split[0].split("-");
-		var final = date_split[1] + "/" + date_split[2] + "/" + date_split[0] + " " + split[1];
+    var split = e.innerHTML.split(" ");
+    var date_split = split[0].split("-");
+    var final = date_split[1] + "/" + date_split[2] + "/" + date_split[0] + " " + split[1];
     var parsed = new Date(final + " UTC").toString().split(" ");
     e.innerHTML = parsed[1] + " " + parsed[2] + ", " + parsed[3] + " " + parsed[4];
 });
