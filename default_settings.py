f3e331004f6fa850807fea8d4f069d9ae34ffe43
cg <31290848+ComputerGenius152@users.noreply.github.com>
2020-12-08 22:08:42 -0500

    fix tests

--- default_settings.py
+++ default_settings.py
@@ -1,9 +1,12 @@
 import secrets
+import sys
+
 try:
     with open('secret_key.txt', 'r') as file:
         secret_key = file.readline().strip()
         SECRET_KEY = secret_key
-except:
+except Exception as e:
+    sys.stderr.write(str(e))
     secret = secrets.token_hex(48)  # 384 bits
     with open('secret_key.txt', 'w+') as file:
         file.write(secret)
@@ -11,7 +14,7 @@ except:
         SECRET_KEY = secret_key
 SESSION_PERMANENT = False
 SESSION_TYPE = "filesystem"
-MAIL_SERVER = "smtp.gmail.com" # configured to work with gmail
+MAIL_SERVER = "smtp.gmail.com"  # configured to work with gmail
 MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = "your email address"
