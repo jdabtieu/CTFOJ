3dd9d6ae76c2d38f4b7bb22378b8797abc6ad576
cg <31290848+ComputerGenius152@users.noreply.github.com>
2020-12-08 21:55:09 -0500

    test for daily_tasks.py

---
+++ tests/test_daily.py
@@ -0,0 +1,14 @@
+import os
+
+import daily_tasks
+
+def test_daily(client):
+    '''Test if daily_tasks.py runs properly and users aren't able to access the secret files.'''
+    assert os.path.exists('secret_key.txt')
+
+    result = client.get('/logs/application.log')
+    assert result.status_code == 404
+    result = client.get('/secret_key.txt')
+    assert result.status_code == 404
+    result = client.get('/database.db')
+    assert result.status_code == 404
