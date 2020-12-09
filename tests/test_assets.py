aa2d80296468c9bc3d8173670128238b7dc4632a
jdabtieu <jonathan.wu3@outlook.com>
2020-12-08 23:43:05 -0500

    Create test_assets.py

---
+++ tests/test_assets.py
@@ -0,0 +1,13 @@
+def test_assets(client):
+    '''Test to ensure assets are visible.'''
+    result = client.get('/assets/css/style.css')
+    assert result.status_code != 404
+    assert b'Page Not Found' not in result.data
+    
+    result = client.get('/assets/images/trash.svg')
+    assert result.status_code != 404
+    assert b'Page Not Found' not in result.data
+    
+    result = client.get('/assets/js/dateconvert.css')
+    assert result.status_code != 404
+    assert b'Page Not Found' not in result.data
