cd4176e43bfe445c7cad44f007bc23de39e86551
Anthony Chen <31290848+ComputerGenius152@users.noreply.github.com>
2020-12-09 00:21:46 -0500

    Update test_assets.py

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -3,11 +3,11 @@ def test_assets(client):
     result = client.get('/assets/css/style.css')
     assert result.status_code == 200
     assert b'Page Not Found' not in result.data
-    
+
     result = client.get('/assets/images/trash.svg')
     assert result.status_code == 200
     assert b'Page Not Found' not in result.data
-    
+
     result = client.get('/assets/js/dateconvert.js')
     assert result.status_code == 200
     assert b'Page Not Found' not in result.data
