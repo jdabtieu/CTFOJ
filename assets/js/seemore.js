2bf5ed3ff34100a7c14d75ca57c6421b7e98e985
Jonathan Wu <jonathan.wu3@outlook.com>
2020-12-22 18:16:34 -0500

    add see more button (fixes #47)

---
+++ assets/js/seemore.js
@@ -0,0 +1,13 @@
+$(() => {
+    $(".see-more").each(function() {
+        if ($(this).parent().parent().find(".showdown").height() < 490) {
+            $(this).parent().remove();
+        }
+    });
+})
+
+$(".see-more").click(function(event) {
+    event.preventDefault();
+    $(event.target).parent().parent().css("max-height", "");
+    $(event.target).parent().remove()
+});
