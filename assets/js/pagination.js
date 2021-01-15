4d2622463632a58b65a16086bc536af6f1249e82
Jonathan Wu <jonathan.wu3@outlook.com>
2021-01-15 14:05:11 -0500

    Remove variable not defined error

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,33 +1,35 @@
-var page = new URL(window.location.href).searchParams.get("page");
-$(() => $('#pagination').twbsPagination({
-    totalPages: total_length,
-    visiblePages: 7,
-    startPage: page ? parseInt(page) : 1,
-    initiateStartPageClick: false,
-    onPageClick: function (event, page) {
-        insertParam(page);
-    }
-}));
-
-function insertParam(value) {
-    key = "page";
-
-    // kvp looks like ['key1=value1', 'key2=value2', ...]
-    var kvp = document.location.search.substr(1).split('&');
-    let i = 0;
-
-    for(; i < kvp.length; i++){
-        if (kvp[i].startsWith('page=')) {
-            let pair = kvp[i].split('=');
-            pair[1] = value;
-            kvp[i] = pair.join('=');
-            break;
+if (typeof total_length !== 'undefined') {
+    var page = new URL(window.location.href).searchParams.get("page");
+    $(() => $('#pagination').twbsPagination({
+        totalPages: total_length,
+        visiblePages: 7,
+        startPage: page ? parseInt(page) : 1,
+        initiateStartPageClick: false,
+        onPageClick: function (event, page) {
+            insertParam(page);
         }
-    }
+    }));
 
-    if(i >= kvp.length){
-        kvp[kvp.length] = ["page", value].join('=');
-    }
+    function insertParam(value) {
+        key = "page";
 
-    document.location.search = kvp.join('&');
+        // kvp looks like ['key1=value1', 'key2=value2', ...]
+        var kvp = document.location.search.substr(1).split('&');
+        let i = 0;
+
+        for(; i < kvp.length; i++){
+            if (kvp[i].startsWith('page=')) {
+                let pair = kvp[i].split('=');
+                pair[1] = value;
+                kvp[i] = pair.join('=');
+                break;
+            }
+        }
+
+        if(i >= kvp.length){
+            kvp[kvp.length] = ["page", value].join('=');
+        }
+
+        document.location.search = kvp.join('&');
+    }
 }
