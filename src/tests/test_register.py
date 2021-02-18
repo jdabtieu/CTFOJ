4d0eb28194df3d86bcc8b69465824d7abcefcfa2
Jonathan Wu <jonathan.wu3@outlook.com>
2021-02-18 12:35:37 -0500

    add tests

--- src/tests/test_register.py
+++ src/tests/test_register.py
@@ -79,6 +79,30 @@ def test_register(client, database):
     assert result.status_code == 409
     assert b'already exists' in result.data
 
+    result = client.post('/register', data={
+        'username': 'randouser',
+        'password': 'testingpass',
+        'confirmation': 'testingpass',
+        'email': 'testingemail@email.com'
+    }, follow_redirects=True)
+
+    assert result.status_code == 409
+    assert b'already exists' in result.data
+
     result = client.get('/confirmregister/fake', follow_redirects=True)
 
     assert b'invalid' in result.data
+
+    # Manually generate token to check
+    exp = datetime.utcnow() - timedelta(seconds=1800)
+    token = jwt.encode(
+        {
+            'email': 'testingemail@email.com',
+            'expiration': exp.isoformat()
+        },
+        'testing_secret_key',
+        algorithm='HS256'
+    )
+
+    result = client.get(f'/confirmregister/{token}')
+    assert b'expired' in result.data
