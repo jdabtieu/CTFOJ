11a907f1473c9b1a78b8d90a7e1b6f5f85136226
Jonathan Wu <jonathan.wu3@outlook.com>
2021-02-17 21:04:08 -0500

    finish experimental dark mode

--- src/assets/js/darkMode.js
+++ src/assets/js/darkMode.js
@@ -1,29 +1,42 @@
-let darkMode = false;
+function setCookie(name, val, exp) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exp*24*60*60*1000)); // expiry in days
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + val + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    name += "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+if (getCookie("darkMode") == 1) enableDark();
 
 function toggleDark() {
-    if (darkMode) disableDark();
-    else enableDark();
-    darkMode = !darkMode;
+    if (getCookie("darkMode") == 1) {
+        disableDark();
+        setCookie("darkMode", 0, 7);
+    } else {
+        enableDark();
+        setCookie("darkMode", 1, 7);
+    }
 }
 
 
 function enableDark() {
     document.body.classList.add("dark");
-    $("input").addClass("dark");
-    $("textarea").addClass("dark");
-    $(".form-control").addClass("dark");
-    $(".set-dark").addClass("dark");
-    $(".card").addClass("dark");
-    $("table").addClass("dark");
-    $(".btn-primary").removeClass("dark");
 }
-
 function disableDark() {
     document.body.classList.remove("dark");
-    $("input").removeClass("dark");
-    $("textarea").removeClass("dark");
-    $(".form-control").removeClass("dark");
-    $(".set-dark").removeClass("dark");
-    $("table").removeClass("dark");
-    $(".card").removeClass("dark");
 }
