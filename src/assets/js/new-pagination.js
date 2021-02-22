b4973fb3054fd1606fb2ada70ff97330654e0211
Jonathan Wu <jonathan.wu3@outlook.com>
2021-02-22 17:27:46 -0500

    use native JS for pagination

---
+++ src/assets/js/new-pagination.js
@@ -0,0 +1,96 @@
+if (typeof total_length !== 'undefined' && total_length > 0) {
+    // get current & total pages
+    var currentPage, totalPages, displayedPages = 3;
+    var page = new URL(window.location.href).searchParams.get("page");
+    currentPage = page ? parseInt(page) : 1;
+    totalPages = total_length;
+
+    // calculate left and right bounds
+    var paginationBegin, paginationEnd;
+    if (2 * displayedPages + 1 > totalPages) {
+       paginationBegin = 1;
+       paginationEnd = totalPages;
+    } else {
+        paginationBegin = currentPage - displayedPages;
+        paginationEnd = currentPage + displayedPages
+        if (paginationBegin <= 0) {
+            let tmp = 1 - paginationBegin;
+            paginationBegin += tmp;
+            paginationEnd += tmp;
+        } else if (paginationEnd > totalPages) {
+            let tmp = paginationEnd - totalPages;
+            paginationBegin -= tmp;
+            paginationEnd -= tmp;
+        }
+    }
+
+    // function to modify page parameter in URI
+    function insertParam(value) {
+        key = "page";
+
+        // kvp looks like ['key1=value1', 'key2=value2', ...]
+        var kvp = document.location.search.substr(1).split('&');
+        let i = 0;
+
+        for(; i < kvp.length; i++){
+            if (kvp[i].startsWith('page=')) {
+                let pair = kvp[i].split('=');
+                pair[1] = value;
+                kvp[i] = pair.join('=');
+                break;
+            }
+        }
+
+        if(i >= kvp.length){
+            kvp[kvp.length] = ["page", value].join('=');
+        }
+
+        document.location.search = kvp.join('&');
+    }
+
+    // determine if we need to disable the First, Prev, Next, and Last buttons
+    var disableFirst = (currentPage == 1 ? "disabled" : "");
+    var disableLast = (currentPage == totalPages ? "disabled" : "");
+
+    // create the pagination tree
+    var e = document.createElement("ul");
+    e.classList.add("pagination");
+    e.innerHTML += `<li class="page-item first ${disableFirst}"><a href="#" class="page-link">First</a></li>`;
+    e.innerHTML += `<li class="page-item prev ${disableFirst}"><a href="#" class="page-link">Prev</a></li>`;
+    for (let i = paginationBegin; i < currentPage; i++) {
+        e.innerHTML += `<li class="page-item"><a href="#" class="page-link">${i}</a></li>`;
+    }
+    e.innerHTML += `<li class="page-item active"><a href="#" class="page-link">${currentPage}</a></li>`;
+    for (let i = currentPage + 1; i <= paginationEnd; i++) {
+        e.innerHTML += `<li class="page-item"><a href="#" class="page-link">${i}</a></li>`;
+    }
+    e.innerHTML += `<li class="page-item next ${disableLast}"><a href="#" class="page-link">Next</a></li>`;
+    e.innerHTML += `<li class="page-item last ${disableLast}"><a href="#" class="page-link">Last</a></li>`;
+
+    // and append the pagination tree to the DOM
+    document.getElementById("pagination").append(e);
+
+    // attach event listeners
+    document.querySelectorAll(".page-item").forEach(e => {
+        if (!(e.classList.contains("active") || e.classList.contains("disabled"))) {
+            e.addEventListener("click", function() {
+                switch (this.innerText) {
+                case "First":
+                    insertParam("1");
+                    break;
+                case "Prev":
+                    insertParam(currentPage - 1);
+                    break;
+                case "Next":
+                    insertParam(currentPage + 1);
+                    break;
+                case "Last":
+                    insertParam(totalPages);
+                    break;
+                default:
+                    insertParam(this.innerText);
+                }
+            });
+        }
+    });
+}
