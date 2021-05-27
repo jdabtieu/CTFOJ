87e7d08f63594e3c3ead42f16ad7986f3165dc75
cg <31290848+slightlyskepticalpotat@users.noreply.github.com>
2021-05-27 01:41:30 -0400

    improve backup

--- src/daily_tasks.py
+++ src/daily_tasks.py
@@ -4,10 +4,23 @@ import datetime
 import os
 import secrets
 import shutil
+import subprocess
 
 # Backup database if exists
 if os.path.exists('database.db'):
     shutil.copy2('database.db', 'database.db.bak')
+
+# backup metadata if exists
+if os.path.exists('metadata'):
+    # remove older folder
+    if os.path.exists('metadata.bak/'):
+        shutil.rmtree('metadata.bak/')
+    shutil.copytree('metadata/', 'metadata.bak/')
+
+# backup directory structure
+result = subprocess.check_output("tree", shell=True).decode()
+with open("structure.bak", "w") as file:
+    file.write(result)
 
 # Generate new secret key
 secret = secrets.token_hex(48)  # 384 bits
