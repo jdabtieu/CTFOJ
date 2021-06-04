91f1ce852a608a1450c7e430781db8229c442a8a
Jonathan Wu <jonathan.wu3@outlook.com>
2021-06-04 19:51:00 -0400

    allow strikethrough

--- src/assets/js/convertmd.js
+++ src/assets/js/convertmd.js
@@ -1,5 +1,6 @@
 var converter = new showdown.Converter();
 converter.setOption('openLinksInNewWindow', true);
+converter.setOption('strikethrough', true);
 for (element of document.getElementsByClassName('showdown')) {
     var toConvert = element.getElementsByTagName('textarea')[0].value;
     element.innerHTML = DOMPurify.sanitize(converter.makeHtml(toConvert), { ADD_ATTR: ['target'] });
