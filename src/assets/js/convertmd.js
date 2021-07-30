682ffae1cd441bbc4c01a5664b49274e928535aa
Jonathan Wu <jonathan.wu3@outlook.com>
2021-07-29 22:59:52 -0400

    Move new sanitization code into new function

--- src/assets/js/convertmd.js
+++ src/assets/js/convertmd.js
@@ -6,6 +6,22 @@ function convertMD(toConvert) {
     return DOMPurify.sanitize(converter.makeHtml(toConvert), { ADD_ATTR: ['target'] });
 }
 
+function inject(targetDiv, content, unhide) {
+    let shadow = targetDiv.attachShadow({mode: "open"});
+    let container = document.createElement("div");
+    container.innerHTML = convertMD(content);
+    shadow.appendChild(container);
+    container.querySelectorAll("*").forEach(e => {
+        if (window.getComputedStyle(e).position === "fixed") {
+            e.style.setProperty("position", "unset", "important");
+        }
+    });
+    if (unhide) {
+        targetDiv.classList.remove("hidden");
+    }
+}
+
+// Deprecated in favor of API + inject function
 for (element of document.getElementsByClassName('showdown')) {
     element.innerHTML = convertMD(element.getElementsByTagName('textarea')[0].value);
 }
