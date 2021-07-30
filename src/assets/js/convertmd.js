180c64dd112cd1f63a43c58c963d3f1f3b5a0ff8
Jonathan Wu <jonathan.wu3@outlook.com>
2021-07-29 23:30:39 -0400

    update edit editorial to use new sanitizer + cleanup

--- src/assets/js/convertmd.js
+++ src/assets/js/convertmd.js
@@ -6,19 +6,27 @@ function convertMD(toConvert) {
     return DOMPurify.sanitize(converter.makeHtml(toConvert), { ADD_ATTR: ['target'] });
 }
 
-function inject(targetDiv, content, unhide) {
-    let shadow = targetDiv.attachShadow({mode: "open"});
+function _inject(shadow, content, editing) {
     let container = document.createElement("div");
     container.innerHTML = convertMD(content);
-    shadow.appendChild(container);
+    shadow.replaceChildren(container);
     container.querySelectorAll("*").forEach(e => {
         if (window.getComputedStyle(e).position === "fixed") {
             e.style.setProperty("position", "unset", "important");
         }
     });
+}
+
+function inject(targetDiv, content, unhide) {
+    let shadow = targetDiv.attachShadow({mode: "open"});
+    _inject(shadow, content);
     if (unhide) {
         targetDiv.classList.remove("hidden");
     }
+}
+
+function injectEditor(targetDiv, content) {
+    _inject(targetDiv.shadowRoot, content);
 }
 
 // Deprecated in favor of API + inject function
