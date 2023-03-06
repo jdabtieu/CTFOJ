e4cbdde4f33dcd425775f388b61279294355af0f
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2023-03-06 13:05:29 -0500

    --amend

--- src/tests/test_captcha.py
+++
@@ -1,8 +0,0 @@
-from helpers import check_captcha
-
-def test_captcha(client, database):
-    result = check_captcha("", "", "")
-    assert b'invalid' in result.data
-
-    result = check_captcha("", "fakeresponse", "")
-    assert b'invalid' in result.data
