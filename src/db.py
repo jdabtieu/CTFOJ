02d2e0b0a1c0879a5dd98715db7ec9df9063d86f
Jonathan Wu <jonathan.wu3@outlook.com>
2023-12-05 23:27:58 -0500

    Temporary fix for cs50/python-cs50#178
    https://github.com/cs50/python-cs50/issues/178

--- src/db.py
+++ src/db.py
@@ -1,5 +1,19 @@
-from cs50 import SQL
+from cs50 import SQL as cs50_SQL
 import sys
+
+class SQL:
+    def __init__(self, url, **kwargs):
+        self.db = cs50_SQL(url, **kwargs)
+
+    def execute(self, query, *args, **kwargs):
+        try:
+            return self.db.execute(query, *args, **kwargs)
+        except ValueError as e:
+            if self.db._autocommit:
+                self.db._autocommit = False
+                self.db.execute("ROLLBACK")
+            raise e
+
 
 
 try:
