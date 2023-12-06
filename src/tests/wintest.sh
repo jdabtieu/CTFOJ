4b0237b266469dd839f7d4925fc23be5b8d9e698
Jonathan Wu <jonathan.wu3@outlook.com>
2023-12-06 00:42:38 -0500

    More cs50 SQL patches

--- src/tests/wintest.sh
+++ src/tests/wintest.sh
@@ -1,6 +1,6 @@
 # Bash script for running tests on Windows
 # Running tests in your working directory will delete your database! Avoid this!
-# WARNING: Make sure you are not already using the /tmp/CTFOJ folder!
+# WARNING: Make sure you are not already using the /tmp/CTFOJ_test folder!
 # This should be run using Git Bash on Windows by executing ./wintest.sh in the tests directory
 # Alternatively, you can test manually however you like.
 
@@ -10,7 +10,7 @@ if [[ $(ls | grep wintest.sh | wc -c) -eq 0 ]]; then
     echo "**STOPPING**"
     exit 1
 fi
-ls /tmp/CTFOJ 2>/dev/null
+ls /tmp/CTFOJ_test 2>/dev/null
 if [[ $? -eq 0 ]]; then
     echo "/tmp/CTFOJ directory exists! Delete it for this test to run."
     echo "**STOPPING**"
@@ -18,22 +18,22 @@ if [[ $? -eq 0 ]]; then
 fi
 
 # Checkout repo
-echo 'Checking out working directory into /tmp/CTFOJ...'
+echo 'Checking out working directory into /tmp/CTFOJ_test...'
 echo 'If cp whines about cannot create symbolic link, ignore it, it is ok'
-cp -r .. /tmp/CTFOJ
-echo 'Checking out working directory into /tmp/CTFOJ...Done!'
+cp -r .. /tmp/CTFOJ_test
+echo 'Checking out working directory into /tmp/CTFOJ_test...Done!'
 
-# Generate secret key (otherwise application.py will throw an error)
-cd /tmp/CTFOJ
-rm -rf logs dl metadata backups secret_key.txt database.db settings.py database.db.bak
-python daily_tasks.py
+echo 'Setting up test environment...'
+cd /tmp/CTFOJ_test
+rm -rf logs dl metadata backups session secret_key.txt database.db settings.py
+echo 'Setting up test environment...Done!'
 
 # Run tests
 echo 'Running tests...'
-python -m pytest -v
+python -m pytest -v tests
 echo 'Running tests...Done!'
 
 # Cleanup
 cd /tmp
-rm -rf CTFOJ
+rm -rf CTFOJ_test
 echo 'Cleanup done!'
