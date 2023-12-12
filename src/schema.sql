3d6448d87b1c3edbc5d88881ae54e69c7f233a5c
Jonathan Wu <jonathan.wu3@outlook.com>
2023-12-12 01:13:50 -0500

    Consolidate SQL schema
    For INSTALL.sh and tests/conftest.py
    Closes #242

---
+++ src/schema.sql
@@ -0,0 +1,85 @@
+CREATE TABLE 'users' (
+    'id' integer PRIMARY KEY NOT NULL,
+    'username' varchar(20) NOT NULL UNIQUE,
+    'password' varchar(64) NOT NULL,
+    'email' varchar(128) UNIQUE,
+    'join_date' datetime NOT NULL DEFAULT(0),
+    'banned' boolean NOT NULL DEFAULT(0),
+    'verified' boolean NOT NULL DEFAULT(0),
+    'twofa' boolean NOT NULL DEFAULT(0),
+    'api' varchar(36) UNIQUE,
+    'total_points' integer NOT NULL DEFAULT(0),
+    'contests_completed' integer NOT NULL DEFAULT(0),
+    'problems_solved' integer NOT NULL DEFAULT(0)
+);
+CREATE TABLE 'user_perms' (
+    'user_id' integer NOT NULL,
+    'perm_id' integer NOT NULL,
+    UNIQUE(perm_id, user_id) ON CONFLICT IGNORE
+);
+CREATE TABLE 'submissions' (
+    'id' integer PRIMARY KEY NOT NULL,
+    'date' datetime NOT NULL DEFAULT(datetime('now')),
+    'user_id' integer NOT NULL,
+    'problem_id' varchar(32) NOT NULL,
+    'contest_id' varchar(32),
+    'correct' boolean NOT NULL,
+    'submitted' text NOT NULL DEFAULT('')
+);
+CREATE TABLE 'problems' (
+    'id' varchar(64) NOT NULL UNIQUE,
+    'name' varchar(256) NOT NULL,
+    'point_value' integer NOT NULL DEFAULT(0),
+    'category' varchar(64),
+    'flag' varchar(256) NOT NULL,
+    'draft' boolean NOT NULL DEFAULT(0),
+    'flag_hint' varchar(256) NOT NULL DEFAULT(''),
+    'instanced' boolean NOT NULL DEFAULT(0)
+);
+CREATE TABLE 'problem_solved' (
+    'user_id' integer NOT NULL,
+    'problem_id' varchar(64) NOT NULL,
+    UNIQUE(problem_id, user_id) ON CONFLICT ABORT
+);
+CREATE TABLE 'contests' (
+    'id' varchar(32) NOT NULL UNIQUE,
+    'name' varchar(256) NOT NULL,
+    'start' datetime NOT NULL,
+    'end' datetime NOT NULL,
+    'scoreboard_visible' boolean NOT NULL DEFAULT(1),
+    'scoreboard_key' varchar(36)
+);
+CREATE TABLE 'announcements' (
+    'id' integer PRIMARY KEY NOT NULL,
+    'name' varchar(256) NOT NULL,
+    'date' datetime NOT NULL
+);
+CREATE TABLE 'contest_users' (
+    'contest_id' varchar(32) NOT NULL,
+    'user_id' integer NOT NULL,
+    'points' integer NOT NULL DEFAULT (0),
+    'lastAC' datetime,
+    'hidden' integer NOT NULL DEFAULT(0),
+    UNIQUE(contest_id, user_id) ON CONFLICT ABORT
+);
+CREATE TABLE 'contest_solved' (
+    'contest_id' varchar(32) NOT NULL,
+    'user_id' integer NOT NULL,
+    'problem_id' varchar(64) NOT NULL,
+    UNIQUE(contest_id, user_id, problem_id) ON CONFLICT ABORT
+);
+CREATE TABLE 'contest_problems' (
+    'contest_id' varchar(32) NOT NULL,
+    'problem_id' varchar(64) NOT NULL,
+    'name' varchar(256) NOT NULL,
+    'point_value' integer NOT NULL DEFAULT(0),
+    'category' varchar(64),
+    'flag' varchar(256) NOT NULL,
+    'draft' boolean NOT NULL DEFAULT(0),
+    'score_min' integer NOT NULL DEFAULT(0),
+    'score_max' integer NOT NULL DEFAULT(0),
+    'score_users' integer NOT NULL DEFAULT(-1),
+    'flag_hint' varchar(256) NOT NULL DEFAULT(''),
+    'instanced' boolean NOT NULL DEFAULT(0),
+    UNIQUE(contest_id, problem_id) ON CONFLICT ABORT
+);
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -31,89 +31,10 @@ def client():
 def database():
     open("database_test.db", "w").close()
     db = SQL("sqlite:///database_test.db")
-    db.execute(
-        ("CREATE TABLE 'users' ("
-         "    'id' integer PRIMARY KEY NOT NULL,"
-         "    'username' varchar(20) NOT NULL UNIQUE,"
-         "    'password' varchar(64) NOT NULL,"
-         "    'email' varchar(128) UNIQUE,"
-         "    'join_date' datetime NOT NULL DEFAULT(0),"
-         "    'banned' boolean NOT NULL DEFAULT(0),"
-         "    'verified' boolean NOT NULL DEFAULT(0),"
-         "    'twofa' boolean NOT NULL DEFAULT(0),"
-         "    'api' varchar(36) UNIQUE,"
-         "    'total_points' integer NOT NULL DEFAULT(0),"
-         "    'contests_completed' integer NOT NULL DEFAULT(0),"
-         "    'problems_solved' integer NOT NULL DEFAULT(0));"))
-    db.execute(
-        ("CREATE TABLE 'user_perms' ("
-         "    'user_id' integer NOT NULL,"
-         "    'perm_id' integer NOT NULL,"
-         "    UNIQUE(perm_id, user_id) ON CONFLICT IGNORE);"))
-    db.execute(
-        ("CREATE TABLE 'submissions' ("
-         "    'id' integer PRIMARY KEY NOT NULL,"
-         "    'date' datetime NOT NULL DEFAULT(datetime('now')),"
-         "    'user_id' integer NOT NULL,"
-         "    'problem_id' varchar(32) NOT NULL,"
-         "    'contest_id' varchar(32),"
-         "    'correct' boolean NOT NULL,"
-         "    'submitted' text NOT NULL DEFAULT(''));"))
-    db.execute(
-        ("CREATE TABLE 'problems' ("
-         "    'id' varchar(64) NOT NULL UNIQUE,"
-         "    'name' varchar(256) NOT NULL,"
-         "    'point_value' integer NOT NULL DEFAULT(0),"
-         "    'category' varchar(64),"
-         "    'flag' varchar(256) NOT NULL,"
-         "    'draft' boolean NOT NULL DEFAULT(0),"
-         "    'flag_hint' varchar(256) NOT NULL DEFAULT(''),"
-         "    'instanced' boolean NOT NULL DEFAULT(0));"))
-    db.execute(
-        ("CREATE TABLE 'problem_solved' ("
-         "    'user_id' integer NOT NULL,"
-         "    'problem_id' varchar(64) NOT NULL,"
-         "    UNIQUE(problem_id, user_id) ON CONFLICT ABORT);"))
-    db.execute(
-        ("CREATE TABLE 'contests' ("
-         "    'id' varchar(32) NOT NULL UNIQUE,"
-         "    'name' varchar(256) NOT NULL,"
-         "    'start' datetime NOT NULL,"
-         "    'end' datetime NOT NULL,"
-         "    'scoreboard_visible' boolean NOT NULL DEFAULT(1),"
-         "    'scoreboard_key' varchar(36));"))
-    db.execute(
-        ("CREATE TABLE 'announcements' ("
-         "    'id' integer PRIMARY KEY NOT NULL,"
-         "    'name' varchar(256) NOT NULL,"
-         "    'date' datetime NOT NULL);"))
-    db.execute(
-        ("CREATE TABLE 'contest_users' ("
-         "    'contest_id' varchar(32) NOT NULL,"
-         "    'user_id' integer NOT NULL,"
-         "    'points' integer NOT NULL DEFAULT (0),"
-         "    'lastAC' datetime,"
-         "    'hidden' integer NOT NULL DEFAULT(0),"
-         "    UNIQUE(contest_id, user_id) ON CONFLICT ABORT);"))
-    db.execute(
-        ("CREATE TABLE 'contest_solved' ("
-         "    'contest_id' varchar(32) NOT NULL,"
-         "    'user_id' integer NOT NULL,"
-         "    'problem_id' varchar(64) NOT NULL,"
-         "    UNIQUE(contest_id, user_id, problem_id) ON CONFLICT ABORT);"))
-    db.execute(
-        ("CREATE TABLE 'contest_problems' ("
-         "    'contest_id' varchar(32) NOT NULL,"
-         "    'problem_id' varchar(64) NOT NULL,"
-         "    'name' varchar(256) NOT NULL,"
-         "    'point_value' integer NOT NULL DEFAULT(0),"
-         "    'category' varchar(64),"
-         "    'flag' varchar(256) NOT NULL,"
-         "    'draft' boolean NOT NULL DEFAULT(0),"
-         "    'score_min' integer NOT NULL DEFAULT(0),"
-         "    'score_max' integer NOT NULL DEFAULT(0),"
-         "    'score_users' integer NOT NULL DEFAULT(-1),"
-         "    'flag_hint' varchar(256) NOT NULL DEFAULT(''),"
-         "    'instanced' boolean NOT NULL DEFAULT(0),"
-         "    UNIQUE(contest_id, problem_id) ON CONFLICT ABORT);"))
+    with open("schema.sql") as schemafile:
+        schema = schemafile.read().split(";")  # split commands by ;
+        schema = [x.strip() for x in schema]  # strip newlines
+        schema = [x for x in schema if x]  # remove empty commands
+    for query in schema:
+        db.execute(query)
     return db
