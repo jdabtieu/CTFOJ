f5af4dad292b50ee6ee17afba2bbeb93ab2b5625
Jonathan Wu <jonathan.wu3@outlook.com>
2023-12-17 23:03:19 -0500

    Permanent fix for cs50 SQL bug

--- src/db.py
+++ src/db.py
@@ -1,24 +1,6 @@
-from cs50 import SQL as cs50_SQL
+from cs50 import SQL
 import sqlalchemy
 import sys
-
-
-# https://github.com/cs50/python-cs50/issues/178
-if int(sqlalchemy.__version__.split('.')[0]) == 1:
-    SQL = cs50_SQL
-else:
-    class SQL:
-        def __init__(self, url, **kwargs):
-            self.db = cs50_SQL(url, **kwargs)
-
-        def execute(self, query, *args, **kwargs):
-            try:
-                return self.db.execute(query, *args, **kwargs)
-            except ValueError as e:
-                if self.db._autocommit:
-                    self.db._autocommit = False
-                    self.db.execute("ROLLBACK")
-                raise e
 
 
 try:
